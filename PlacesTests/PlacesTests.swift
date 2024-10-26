70599d50e6560722c110fa71b3dd9df5742a1105
Divine Dube <divine@backbase.com>
2024-10-26 14:50:45 +0300

    Initial Commit

---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Divine Dube on 26/10/2024.
+//
+
+import Testing
+@testable import Places
+
+struct PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
